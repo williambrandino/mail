25bd5f7bed37c7271885de4b737bd70c7861fbfa
williambrandy <williambrandy@gmail.com>
2016-07-01 20:25:43 -0300

    Primeiro Branch de Mail Marketing

---
+++ src/App/Action/TestePageAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Template\TemplateRendererInterface;
+
+class TestePageAction
+{
+
+    private $template;
+
+    public function __construct(TemplateRendererInterface $template = null)
+    {
+        $this->template = $template;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+    {
+        return new HtmlResponse($this->template->render("app::teste",['data' => 'Minha primeira aplicação']));
+    }
+}
